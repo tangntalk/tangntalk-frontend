342d6b77934744d2d75d8b35542a433085172bd2
mumwa <mumwa@naver.com>
2021-10-31 10:08:03 +0900

    Header Icon

--- src/components/hearder.js
+++ src/components/hearder.js
@@ -1,15 +1,68 @@
+import { withRouter } from 'react-router-dom';
+
 import styled from "styled-components";
+import BackIcon from "./icon/Back"
+import SearchIcon from "./icon/Search"
 
 function Header(props) {
-    <Space>
-
-    </Space>
+    return (
+        <HeaderSpace>
+            <HeaderContent>
+                <Wrapper>
+                    {props.back&&<BackIcon> </BackIcon>}
+                    <Title>
+                        {props.title}
+                    </Title>
+                </Wrapper>
+                <Wrapper>
+                    {props.search&&<SearchIcon></SearchIcon>}   
+                </Wrapper>
+            </HeaderContent>
+        </HeaderSpace>
+    );
 }
 
-export default Header;
+export default withRouter(Header);
 
-const Space = styled.div`
+export const HeaderSpace = styled.div`
     width:100%;
     height:60px;
-    box-shadow: 5px 10px;
+    box-sizing: border-box;
+
+    position:fixed;
+    top:0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color:#ffffff;
+`
+
+export const HeaderContent = styled.div`
+    width:100%;
+    max-width:800px;
+    height:60px;
+    box-sizing: border-box;
+
+    display: flex;
+    align-items: center;
+    justify-content:space-between;
+
+    @media only screen and (max-width: 768px) {
+        width:100%;
+    }
+`
+export const Wrapper = styled.div`
+    padding:0 10px;
+    display: flex;
+    align-items: center;
+`
+
+export const Title = styled.div`
+    word-break:keep-all;
+    font-size:1.5em;
+    font-weight:bold;
+    overflow:hidden;
+    margin-left:10px;
 `
---
+++ src/components/icon/Chatting.js
@@ -0,0 +1,28 @@
+import { withRouter } from 'react-router-dom';
+
+import { Icon } from "../../styles/style";
+import styled from "styled-components";
+
+function ChattingIcon(props) {
+    const goChat = () => props.history.push('/chat');
+    return (
+        <Icon onClick={goChat}>
+            <svg width="25" height="24" viewBox="0 0 25 24" fill="#948EA5" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.16035 21V5C3.16035 3.89543 4.05579 3 5.16035 3H19.1604C20.2649 3 21.1604 3.89543 21.1604 5V15C21.1604 16.1046 20.2649 17 19.1604 17H9.16035C8.72749 16.9992 8.30617 17.1396 7.96035 17.4L3.16035 21ZM5.16035 5V17L7.29435 15.4C7.64 15.1393 8.06143 14.9988 8.49435 15H19.1604V5H5.16035Z" />
+            </svg>
+            <Text>
+                채팅 목록
+            </Text>
+        </Icon>
+    );
+}
+
+export default withRouter(ChattingIcon);
+
+export const Text = styled.div`
+    word-break:keep-all;
+    font-size:0.8em;
+    font-weight:bold;
+    overflow:hidden;
+    color:#948EA5;
+`
