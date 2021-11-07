d4870aa5033d08a5f7d8395d9b589f9413d19b34
mumwa <mumwa@naver.com>
2021-11-08 01:13:14 +0900

    Fix Header

--- src/pages/chatList.js
+++ src/pages/chatList.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Header from "../components/Hearder";
+import Header from "../components/Header";
 import { ContainerSpace, ContainerContent } from "../styles/style";
 import NaviBar from "../components/NaviBar";
 
