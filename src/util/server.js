55c32ee5e8fe7d9df09648b377bd72a3cde2dcc3
Hoyong Jung <hoyong.jung.dev@gmail.com>
2021-11-12 23:58:50 +0900

    fix CORS error\nchange server url to localhost

--- src/util/server.js
+++ src/util/server.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const server = axios.create();
-server.defaults.baseURL = "http://54.180.165.50:8080";
+server.defaults.baseURL = "http://localhost:8080";
 
 export default server;
