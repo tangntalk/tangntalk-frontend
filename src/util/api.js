dcd364f8febc1afe78a913297c81cbba8d7446c5
mumwa <mumwa@naver.com>
2022-04-04 15:55:22 +0900

    Fix util/api.js

--- src/util/api.js
+++ src/util/api.js
@@ -1,25 +1,27 @@
 import server from "./server";
 
 // User
-export const login = (id, password) => server.post('/login', { user_id: id, password: password });
-export const register = (id, password, name, type) => server.post('/register', { user_id: id, password: password, name: name, type: type });
-export const user = (user_id) => server.get(`/users/${user_id}`);
-export const userDelete = (user_id) => server.delete(`/users/${user_id}`);
-export const userLocation = (user_id, location_name) => server.post(`/users/${user_id}/location`, { location_name: location_name});
-export const userStatus = (user_id, status_message) => server.post(`/users/${user_id}/status`, { status_message: status_message });
-export const logout = (user_id) => server.post(`${user_id}/logout`);
+export const login = (account_id, password) => server.post('/login', { account_id: account_id, password: password });
+export const register = (account_id, password, name, type) => server.post('/register', { account_id: account_id, password: password, name: name, type: type });
+export const user = (account_id) => server.get(`/accounts/${account_id}`);
+export const userDelete = (account_id) => server.delete(`/accounts/${account_id}`);
+export const userLocation = (account_id, location_name) => server.post(`/accounts/${account_id}`, { location_name: location_name});
+export const userStatus = (account_id, status_message) => server.post(`/accounts/${account_id}`, { status_message: status_message });
+export const logout = (account_id) => server.post(`${account_id}/logout`);
 
 // Friend
-export const friendList = (user_id) => server.get(`/users/${user_id}/friends`);
-export const friendAdd = (user_id, friend_id) => server.post(`/users/${user_id}/friends`, { friend_id: friend_id });
-export const friendSearch = (user_id, search) => server.get(`/users/${user_id}/friends/search?query=${search}`);
-export const friendDelete = (user_id, friend_id) => server.delete(`/users/${user_id}/friends/${friend_id}`);
-export const friendNearby = (user_id, location) => server.get(`/users/${user_id}/nearby/${location}`);
-export const friendCheck = (user_id, friend_id) => server.get(`/users/${user_id}/friends/${friend_id}`);
+export const friendCheck = (account_id, friend_id) => server.get(`/accounts/${account_id}/friends/${friend_id}`);
+
+export const friendList = (account_id) => server.get(`/accounts/${account_id}/friends`);
+export const friendAdd = (account_id, friend_id) => server.post(`/accounts/${account_id}/friends`, { friend_id: friend_id });
+export const friendSearch = (account_id, search) => server.get(`/accounts/${account_id}/friends/search?query=${search}`);
+export const friendDelete = (account_id, friend_id) => server.delete(`/accounts/${account_id}/friends/${friend_id}`);
+export const friendNearby = (account_id, location) => server.get(`/accounts/${account_id}/nearby/${location}`);
+
 
 // Chat
-export const chatroomList = (user_id) => server.get(`/users/${user_id}/chatrooms`);
-export const chatroomEnter = (user_id, opponent_id) => server.post(`/users/${user_id}/chatrooms`, { opponent_id: opponent_id});
-export const chatList = (user_id, chatroom_id) => server.get(`/users/${user_id}/chatrooms/${chatroom_id}`);
-export const chatSend = (user_id, chatroom_id, content, rendezvous_time) => server.post(`/users/${user_id}/chatrooms/${chatroom_id}`, {content: content, rendezvous_time: rendezvous_time});
-export const messageCount = (user_id, chatroom_id) => server.get(`users/${user_id}/chatrooms/${chatroom_id}/count`);
+export const chatroomList = (account_id) => server.get(`/users/${account_id}/chatrooms`);
+export const chatroomEnter = (account_id, chatroom_id) => server.get(`/users/${account_id}/chatrooms/${chatroom_id}`);
+export const chatList = (account_id) => server.get(`/users/${account_id}/chatrooms`);
+export const chatSend = (account_id, chatroom_id, content, rendezvous_time) => server.post(`/users/${account_id}/chatrooms/${chatroom_id}`, {content: content, rendezvous_time: rendezvous_time});
+export const messageCount = (account_id, chatroom_id) => server.get(`/${account_id}/chatrooms/${chatroom_id}/count`);
